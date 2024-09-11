dd3d34ebe9caeb3774841a6d9812a2f410484666
abiola <abiolafadeyi10@gmail.com>
2024-09-11 20:13:43 +0100

    Crated question endpoints

--- Backend/StudyAssistant/users/urls.py
+++ Backend/StudyAssistant/users/urls.py
@@ -1,4 +1,5 @@
 # urls.py
+from rest_framework.authtoken.views import obtain_auth_token
 from django.urls import path
 from .views import RegisterUserView, LoginUserView, LogoutUserView, UserDetailView, UserListView
 
@@ -8,5 +9,6 @@ urlpatterns = [
     path('api/users/', UserListView.as_view(), name='users'),
     path('api/register/', RegisterUserView.as_view(), name='api_register'),
     path('api/login/', LoginUserView.as_view(), name='api_login'),
+    path('api-token-auth/', obtain_auth_token),
     path('api/logout/', LogoutUserView.as_view(), name='api_logout'),
 ]
